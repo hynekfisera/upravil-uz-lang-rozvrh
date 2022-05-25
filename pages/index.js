a1e3b0c3a4cb4efd32bc41dec93a5d8effc6a70c
Hynek Fišera <53520774+hynekfisera@users.noreply.github.com>
2022-05-25 07:18:46 +0200

    Init

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Upravil už Lang rozvrh?</title>
+      </Head>
+      <h1>Ne.</h1>
+    </>
+  );
+}
