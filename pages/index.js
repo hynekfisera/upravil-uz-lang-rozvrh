904c6d17ff87f985f3ea45e2a9bbc8deea94e762
Hynek Fišera <53520774+hynekfisera@users.noreply.github.com>
2022-05-25 07:26:58 +0200

    Get text by current hour

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,26 @@
 import Head from "next/head";
 
 export default function Home() {
+  const getText = () => {
+    const date = new Date();
+    const hours = date.getHours();
+    if (hours >= 16 && hours <= 20) {
+      return "Asi ne";
+    } else if (hours >= 21 && hours <= 23) {
+      return "Už by mohl";
+    } else if (hours >= 0 && hours <= 6) {
+      return "Měl by";
+    } else {
+      return "Ne.";
+    }
+  };
+
   return (
     <>
       <Head>
         <title>Upravil už Lang rozvrh?</title>
       </Head>
-      <h1>Ne.</h1>
+      <h1>{getText()}</h1>
     </>
   );
 }
